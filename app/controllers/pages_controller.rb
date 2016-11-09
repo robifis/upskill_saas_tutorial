0a858e36da13b2d03f6edaaf0185891e3ebc2b46
Robert Olejnik <your.email@address.comrobert.olejnik.1986@gmail.com>
2016-11-09 15:03:57 +0000

    Added contact, view route, controller action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
 class PagesController < ApplicationController 
-  def home
+  def home #/ This is how you add the first homepage that will then be stored inside the pages directory
   end
   
-  def about
+  def about #/This is what the name of the page you're adding is called
+  end
+  
+  def contact #/I am now trying to add a contact page to see if that will work
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do 
   root to: 'pages#home'
   get 'about', to: 'pages#about' #/This refers to the pages contoller file
+  get 'contact', to: 'pages#contact' #/ This is the contact page
 end
